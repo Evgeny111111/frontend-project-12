5523d47d36b868b4fc2786bbfe079339c89233a0
Evgenii <rasshivalov95@mail.ru>
2025-03-15 19:36:16 +0300

    fix button add

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -87,8 +87,7 @@ const HomePage = () => {
                 }))}
                 variant="link"
                 className="p-0 text-primary"
-                aria-label="Добавить канал"
-                title="Добавить канал"
+                aria-label="+"
               >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -101,6 +100,7 @@ const HomePage = () => {
                   <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4" />
                 </svg>
               </Button>
+
             </div>
             <Channels />
           </Col>
