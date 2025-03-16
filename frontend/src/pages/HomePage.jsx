f449859b32e3f065cb028290031e7941159f3d2d
Evgenii <rasshivalov95@mail.ru>
2025-03-16 09:44:30 +0300

    check

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -85,23 +85,26 @@ const HomePage = () => {
                   modalShow: true,
                   modalType: 'adding',
                 }))}
-                variant="link"
-                className="p-0 text-primary"
+                className="d-flex align-items-center justify-content-center"
+                style={{
+                  width: '20px',
+                  height: '20px',
+                  fontSize: '16px',
+                  fontWeight: 'bold',
+                  background: 'transparent',
+                  border: '1px solid black',
+                  padding: 0,
+                  lineHeight: '16px',
+                  userSelect: 'none',
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  borderRadius: '0',
+                  color: 'black',
+                }}
                 aria-label="Добавить канал"
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 16 16"
-                  width="20"
-                  height="20"
-                  fill="currentColor"
-                >
-                  <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z" />
-                  <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4" />
-                </svg>
-                <span className="visually-hidden">
-                  +
-                </span>
+                <span aria-hidden="true">+</span>
               </Button>
 
             </div>
