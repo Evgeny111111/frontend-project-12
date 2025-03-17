0b1b072e57a5d1abea0613f3f54374bf318e3f8e
Evgenii <rasshivalov95@mail.ru>
2025-03-17 07:35:14 +0300

    fix butt

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -81,12 +81,12 @@ const HomePage = () => {
             <div className="d-flex mt-1 justify-content-between mb-2 ps-4 pe-2 p-4">
               <b>{t('channels.title')}</b>
               <Button
+                className="p-0 text-primary btn btn-group-vertical"
                 onClick={() => dispatch(changeModalShow({
                   modalShow: true,
                   modalType: 'adding',
                 }))}
                 variant="link"
-                className="p-0 text-primary"
                 aria-label="Добавить канал"
               >
                 <svg
@@ -99,9 +99,9 @@ const HomePage = () => {
                   <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z" />
                   <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4" />
                 </svg>
-                <span className="visually-hidden">+</span>
-                {' '}
-                {/* скрываем + визуально, но Playwright его видит */}
+                <span className="visually-hidden">
+                  +
+                </span>
               </Button>
 
             </div>
