093b5a9c17f26836e35a4bedab59e4f245bf2976
Evgenii <rasshivalov95@mail.ru>
2025-03-19 19:08:29 +0300

    svg

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -13,6 +13,7 @@ import { selectCurrentChannel } from '../store/slices/channelsSlices';
 import { useGetMessagesQuery } from '../API/messages';
 import { useGetChannelsQuery } from '../API/channels';
 import { changeModalShow } from '../store/slices/modalsSlices';
+import SendIcon from './SendIcon';
 
 const SpinnerPage = () => (
   <Container
@@ -89,21 +90,11 @@ const HomePage = () => {
                 variant="link"
                 aria-label="Добавить канал"
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 16 16"
-                  width="20"
-                  height="20"
-                  fill="currentColor"
-                >
-                  <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z" />
-                  <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4" />
-                </svg>
+                <SendIcon width={20} height={20} />
                 <span className="visually-hidden">
                   +
                 </span>
               </Button>
-
             </div>
             <Channels />
           </Col>
